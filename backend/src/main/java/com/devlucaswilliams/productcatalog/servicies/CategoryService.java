0b2be556ec5d43a00bee5e0d3bcf2309e81b1a4f
Lucas williams <lucaswilliamscesar@gmail.com>
2025-03-10 19:57:28 -0300

    seeding database

--- backend/src/main/java/com/devlucaswilliams/productcatalog/servicies/CategoryService.java
+++ backend/src/main/java/com/devlucaswilliams/productcatalog/servicies/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devlucaswilliams.productcatalog.entities.Category;
 import com.devlucaswilliams.productcatalog.repositories.CategoryRepository;
@@ -14,6 +15,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		List<Category> list = repository.findAll();
 		return list;

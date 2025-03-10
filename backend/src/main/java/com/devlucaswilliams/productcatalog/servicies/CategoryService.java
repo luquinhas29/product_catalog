9304dd8fc035412ca4df9231c8870b1f61651983
Lucas williams <lucaswilliamscesar@gmail.com>
2025-03-10 20:24:30 -0300

    CategoryDTO

--- backend/src/main/java/com/devlucaswilliams/productcatalog/servicies/CategoryService.java
+++ backend/src/main/java/com/devlucaswilliams/productcatalog/servicies/CategoryService.java
@@ -1,11 +1,13 @@
 package com.devlucaswilliams.productcatalog.servicies;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devlucaswilliams.productcatalog.dto.CategoryDTO;
 import com.devlucaswilliams.productcatalog.entities.Category;
 import com.devlucaswilliams.productcatalog.repositories.CategoryRepository;
 
@@ -16,8 +18,8 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
+	public List<CategoryDTO> findAll(){
 		List<Category> list = repository.findAll();
-		return list;
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 }
